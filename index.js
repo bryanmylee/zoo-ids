ecbe0d57a486f67387552fe9d465ed2e5debcdea
Bryan Lee <bryanleeminyuan@gmail.com>
2020-05-07 04:22:22 +0800

    Move options into an optional object
    Fix bug where multi-word adjectives were not separated by the specified
    delimiter.

--- index.js
+++ index.js
@@ -4,30 +4,33 @@ const animals = require('./animals');
 
 /**
  *
- * @param {string | number} [seed=0] The value to seed the generated ID with
+ * @param {*} [seed=0] The value to seed the generated ID with
  * @param {number} [adjectiveCount=2] The number of adjectives to append
  * @param {string} [delimiter=''] The string delimiter between words
  * @param {('titlecase'|'camelcase'|'uppercase'|'lowercase'|'togglecase')} [caseStyle='titlecase'] The case style for words
  */
-function generateId(seed, adjectiveCount, delimiter, caseStyle) {
+function generateId(seed, adjectiveCount, opts) {
   if (seed == null) seed = 0;
+  if (opts == null) opts = {
+    delimiter: '',
+    caseStyle: 'titlecase',
+  }
   if (adjectiveCount == null) adjectiveCount = 2;
-  if (delimiter == null) delimiter = '';
-  if (caseStyle == null) caseStyle = 'titlecase';
+  if (opts.delimiter == null) opts.delimiter = '';
+  if (opts.caseStyle == null) opts.caseStyle = 'titlecase';
 
-  // Prevent increases in the adjective count merely appending new adjectives
-  const rng = alea(seed + adjectiveCount);
+  const rng = alea(seed);
 
   let result = '';
   let index;
   while (--adjectiveCount >= 0) {
     index = getRandomIndex(rng, adjectives.length);
-    result += getFormattedWord(adjectives[index], caseStyle) + delimiter;
+    result += getFormattedWord(adjectives[index], opts) + opts.delimiter;
   }
   index = getRandomIndex(rng, animals.length);
-  result += getFormattedWord(animals[index], caseStyle);
+  result += getFormattedWord(animals[index], opts);
 
-  if (caseStyle === 'camelcase') {
+  if (opts.caseStyle === 'camelcase') {
     return result.charAt(0).toLowerCase() + result.slice(1);
   }
   return result;
@@ -38,9 +41,9 @@ function getRandomIndex(rng, length) {
   return index === length ? index - 1 : index;
 }
 
-function getFormattedWord(word, caseStyle) {
-  if (word.includes('-')) return getFormattedWords(word.split('-'), caseStyle);
-  switch (caseStyle) {
+function getFormattedWord(word, opts) {
+  if (word.includes('-')) return getFormattedWords(word.split('-'), opts);
+  switch (opts.caseStyle) {
     case 'titlecase': case 'camelcase':
       return word.charAt(0).toUpperCase() + word.slice(1);
     case 'uppercase':
@@ -52,10 +55,10 @@ function getFormattedWord(word, caseStyle) {
   }
 }
 
-function getFormattedWords(words, caseStyle) {
+function getFormattedWords(words, opts) {
   return words.map(function(word) {
-    return getFormattedWord(word, caseStyle)
-  }).join();
+    return getFormattedWord(word, opts)
+  }).join(opts.delimiter);
 }
 
 function getToggleCaseWord(word) {
