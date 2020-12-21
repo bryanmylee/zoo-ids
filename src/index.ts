37e7f2b2d4794dfd7c6c7e531a887c33213620f6
Bryan Lee <bryanmylee@gmail.com>
2020-12-21 13:30:10 +0800

    feat: unuse class definition and cache generator only

--- src/index.ts
+++ src/index.ts
@@ -10,76 +10,58 @@ interface GenerateIdOptions {
   caseStyle: CaseStyle;
 }
 
-export default class IdGenerator {
+const defaultOptions: GenerateIdOptions = {
+  numAdjectives: 2,
+  delimiter: '',
+  caseStyle: 'titlecase',
+};
 
-  private generator: RandomGenerator;
-  private options: GenerateIdOptions;
+const cachedGenerator: RandomGenerator = Alea(new Date());
 
-  constructor(seed: any = null, {
-    numAdjectives = 2,
-    delimiter = '',
-    caseStyle = 'titlecase',
-  }: Partial<GenerateIdOptions> = {}) {
-    if (seed == null) {
-      seed = new Date();
-    }
-    this.generator = Alea(seed);
-    this.options = {
-      numAdjectives,
-      delimiter,
-      caseStyle,
-    };
+export function generateId(seed: any = null, options: Partial<GenerateIdOptions> = {}) {
+  const generator = seed == null ? cachedGenerator : Alea(seed);
+  const fullOptions = { ...defaultOptions, ...options };
+  const { numAdjectives, delimiter, caseStyle } = fullOptions;
+  let result = '';
+  for (let i = 0; i < numAdjectives; i++) {
+    const adjective = getRandomElement(generator, adjectives);
+    result += getFormattedWord(adjective, { numAdjectives, delimiter, caseStyle });
+    result += options.delimiter;
   }
-
-  public generateId(customOptions: Partial<GenerateIdOptions> = {}) {
-    const options = {
-      ...this.options,
-      ...customOptions,
-    };
-    let result = '';
-    for (let i = 0; i < options.numAdjectives; i++) {
-      const adjective = this.getRandomElement(adjectives);
-      result += this.getFormattedWord(adjective, options);
-      result += options.delimiter;
-    }
-    const animal = this.getRandomElement(animals);
-    result += this.getFormattedWord(animal, options);
-    if (options.caseStyle === 'camelcase') {
-      return result.charAt(0).toLowerCase() + result.slice(1);
-    }
-    return result;
+  const animal = getRandomElement(generator, animals);
+  result += getFormattedWord(animal, fullOptions);
+  if (caseStyle === 'camelcase') {
+    return result.charAt(0).toLowerCase() + result.slice(1);
   }
-
-  private getRandomElement(words: string[]) {
-    const index = Math.floor(this.generator() * words.length);
-    return words[Math.min(index, words.length - 1)];
-  }
-
-  private getFormattedWord(word: string, options: GenerateIdOptions): string {
-    if (word.includes('-')) {
-      return word.split('-')
-          .map(w => this.getFormattedWord(w, options))
-          .join(options.delimiter);
-    }
-    switch (options.caseStyle) {
-      case 'titlecase': case 'camelcase':
-        return word.charAt(0).toUpperCase() + word.slice(1);
-      case 'uppercase':
-        return word.toUpperCase();
-      case 'togglecase':
-        return this.getToggleCaseWord(word);
-      case 'lowercase': default:
-        return word;
-    }
-  }
-
-  private getToggleCaseWord(word: string) {
-    return word.split('').map(c => this.generator() > 0.5 ? c.toUpperCase() : c).join('');
-  }
-
+  return result;
 }
 
-const idGenerator = new IdGenerator();
+function getRandomElement(generator: RandomGenerator, words: string[]) {
+  const index = Math.floor(generator() * words.length);
+  return words[Math.min(index, words.length - 1)];
+}
 
-export const generateId = idGenerator.generateId;
+function getFormattedWord(word: string, options: GenerateIdOptions): string {
+  if (word.includes('-')) {
+    return word.split('-')
+        .map(w => getFormattedWord(w, options))
+        .join(options.delimiter);
+  }
+  switch (options.caseStyle) {
+    case 'titlecase': case 'camelcase':
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    case 'uppercase':
+      return word.toUpperCase();
+    case 'togglecase':
+      return getToggleCaseWord(word);
+    case 'lowercase': default:
+      return word;
+  }
+}
+
+function getToggleCaseWord(word: string) {
+  return word.split('')
+      .map((c, i) => i % 2 === 0 ? c.toUpperCase() : c)
+      .join('');
+}
 
